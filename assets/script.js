8d29c8fb0ba060e90764c4b245436024ac2097ad
Jett <jettc505@gmail.com>
2022-10-23 03:35:22 -0600

    Working with the api

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,14 @@ const API_KEY = "9d35e655e95e27d60138ab4d4be043b1";
 const City = "Denver"
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${API_KEY}`;
 
-fetch(API_URL)
-    .then((data) => data.json())
-    .then((data) => console.log(data))
+function tempfun() {
+    // will be removed
+    fetch(API_URL)
+        .then((data) => data.json())
+        .then((data) => console.log(data));
+        
+    console.log("temp is")
+}
 
 function searchBtnClick() {
     // What happens when the button is clicked
@@ -17,9 +22,10 @@ function searchBtnClick() {
 
 function saveSearchHistory(search) {
     // saves the users input to localStorage
-
+    // TODO: Use Template Literals to display the search history button 
 }
 
 function displayData(day) {
     // displays the weather for the day
 }
+tempfun()
